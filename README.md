93023e68c02104dcfcd9adb18d30b73b69408195
Tom Nicholas <tomblyth90@gmail.com>
2023-02-23 12:17:21 +1100

    major updates to all files

--- README.md
+++ README.md
@@ -1,31 +1,29 @@
 # project1 : tic-tac-toe
 
+To play the game [click here](https://bigbbazz.github.io/project1/)
+
+To view the wireframe [click here](https://whimsical.com/tic-tac-toe-wireframe-Ay9LEvSSwmaQJgogJqu3mE)
+
+To view the flow-chart [click here](https://whimsical.com/tic-tac-toe-QBMr1vPh4ibRfST4nXXFDE)
 
 ## Technologies
 
-Technical: HTML, CSS, JavaScript
+Languages: HTML, CSS, JavaScript
+
+Editing: Visual Studio Code, Google Chrome
 
 Project Managment: Excel, whimsical.com
 
 ## Approach
 
-Step 1:
+**Step 1:** Made git repo for the project
 
-Made git repo for the project
+**Step 2:** Created project managment spreadsheet to document progress
 
-Step 2:
+**Step 3:** Created wireframe and flow-chart using whimsical.com
 
-Created project managment spreadsheet to document progress
+**Step 4:** 
 
-Step 3:
-
-Created descision flow-chart using whimsical.com
-
-Created wireframe of webpage
-
-Step 4:
-
-## Project Managment
 
 
 
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,46 +9,120 @@
 // Latest edit date:
 // =====================================
 
-
-
 const grid = document.querySelector('.grid-parent');
 
 const buttons = document.querySelectorAll('.grid-child');
 
-let roundNumber = 1;
+const resetButton = document.querySelector('.reset');
 
-let player1buttons = [];
+const rounds = document.querySelector('.rounds');
 
-let player2buttons = [];
+const message = document.querySelector('.message');
 
-winnerArrays = {
+let roundCounter = 0;
 
-    winArray1 : [1,2,3],
-    winArray2 : [1,5,9],
-    winArray3 : [1,4,7],
-    winArray4 : [2,5,8],
-    winArray5 : [3,6,9],
-    winArray6 : [3,5,7]
-}
+let player1Selections = [1,2,3];
+
+let player2Selections = [];
+
+const winnerArrays = [[1,2,3],[1,5,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]];
 
 let buttonNumber = 1;
 
+
+
 buttons.forEach(button => {
 
-    button.setAttribute('data-buttonNumber',buttonNumber);
+    button.setAttribute('data-gridNumber',buttonNumber);
     buttonNumber++;
 });
 
+function addSelection(event){
 
-function winnerArrayFunc () {
+    let buttonClicked = event.target;
+    
+    if (roundCounter % 2 !== 0) {
 
-    if (roundNumber % 2 == 0) {
+        player1Selections.push(Number(buttonClicked.dataset.gridnumber));
+    } else {
 
-        player1buttons.push(buttonClicked.dataset.buttonNumber)
+        player2Selections.push(Number(buttonClicked.dataset.gridnumber));
     }
 }
 
-function handleUserClick (event) {
+
+function checkForWinner() {
+
+    if (roundCounter < 5){
+
+        return false;
+    }
+
+    if (roundCounter % 2 !== 0) {
+        
+        if (player1Selections.length>=3) {
+
+            let winArrIndex = 0; 
+
+            while(winArrIndex<winnerArrays.length) {
+
+                let matchCounter = 0;
+    
+                for (let player1Selection of player1Selections) {
+                    
+                    if (winnerArrays[winArrIndex].indexOf(player1Selection)>-1) {
+
+                        matchCounter++;
+        
+                        if (matchCounter == 3) {
+
+                            return true;
+                        }
+                    }                       
+                }
+
+                winArrIndex++;
+            }               
+        } 
+    } else {
+
+        if (player2Selections.length>=3) {
+
+            let winArrIndex = 0; 
+
+            while(winArrIndex<winnerArrays.length) {
+
+                let matchCounter = 0;
+    
+                for (let player2Selection of player2Selections) { //foreach doesnt work, won't return at line 105 if matchCounter ==3
+                    
+                    if (winnerArrays[winArrIndex].indexOf(player2Selection)>-1) {
+
+                        matchCounter++;
+        
+                        if (matchCounter == 3) {
+
+                            return true;
+                        }
+                    }      
+                }
+
+                winArrIndex++;
+            }                        
+        } 
+    }    
+}
+
+function checkForDraw() {
+    
+    if (roundCounter == 9){
+
+    message.textContent = `It's a draw!!`;
+    message.style.visibility = 'visibile';
+}
+}
+
+function handleUserClick(event) {
 
     let buttonClicked = event.target;
 
@@ -59,19 +133,57 @@ function handleUserClick (event) {
 
     buttonClicked.setAttribute('disabled',true);
 
-    roundNumber++;
+    roundCounter++;
 
-    buttonClicked.classList.toggle("player1", roundNumber % 2 == 0);
+    buttonClicked.classList.toggle("player1", roundCounter % 2 !== 0);
 
-    buttonClicked.classList.toggle("player2", roundNumber % 2 != 0);
-    
+    buttonClicked.classList.toggle("player2", roundCounter % 2 == 0);
 }
 
+buttons.forEach(button => {
 
-// button.addEventListener('click', handleUserClick)
+    button.addEventListener('click', (event) => {
 
-grid.addEventListener('click', handleUserClick)
+        handleUserClick(event);
+    
+        addSelection(event);
+    
+        rounds.textContent = `Round number: ${roundCounter}`;
+    
+        rounds.style.visibility = 'visible';
+    
+        if(checkForWinner()) {
+    
+            resetButton.style.visibility = 'visible';
+    
+            if (roundCounter % 2 !== 0) {
+    
+                message.textContent = `Player 1 - WINNER`;
+            } else {
+    
+                message.textContent = `Player 2 - WINNER`;   
+            } 
+        };
 
+        checkForDraw();
+    })  
+});
+
+
+resetButton.addEventListener('click', () => {
+
+let roundCounter = 0;
+
+let player1Selections = [];
+
+let player2Selections = [];
+
+rounds.style.visibility = 'hidden';
+
+message.style.visibility = 'hidden';
+
+resetButton.style.visibility = 'hidden';
+})
 
 
 
